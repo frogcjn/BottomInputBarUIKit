f2c593e3f99edb2d624ad90eece0b8dde756ea34
frogcjn <frogcjn@163.com>
2024-01-11 11:45:21 -0500

    recover SafeAreaGuideview

--- BottomInputBarUIKit/KeyboardLayoutGuideView.swift
+++ BottomInputBarUIKit/KeyboardLayoutGuideView.swift
@@ -50,3 +50,65 @@ class KeyboardLayoutGuideView : UIView {
         ])
     }
 }
+
+
+class SafeAreaGuideView : UIView {
+    let guideView: UIView!
+    init() {
+        
+        self.guideView = {
+            let guideView = UIView()
+            guideView.layer.borderColor = UIColor.red.cgColor
+            guideView.layer.borderWidth = 1
+            guideView.backgroundColor = .red.withAlphaComponent(0.5)
+            guideView.translatesAutoresizingMaskIntoConstraints = false
+            guideView.isUserInteractionEnabled = false
+            return guideView
+        }()
+        
+        super.init(frame: .zero)
+        
+        // configure self
+        backgroundColor = .yellow
+        isUserInteractionEnabled = false
+        
+        // add subviews
+        addSubview(guideView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        
+        NSLayoutConstraint.deactivate(guideView.constraints)
+        guard let superview = superview?.superview else { return }
+
+        /*
+        guard let rootView1 = superview?.superview else { return }
+        guard let rootView2 = superview?.superview?.superview else { return }
+
+        //print(1, superview) // BottomBar.HosingController, and its view
+        //print(2, superview?.superview) // view Problem!
+        //print(3, superview?.superview?.superview) // SwiftUI.NavigationStackHostingController, and its view
+
+        //superview?.superview?.backgroundColor = .black
+        //rootView.keyboardLayoutGuide.usesBottomSafeArea = false
+        superview?.clipsToBounds = false
+        rootView1.clipsToBounds = false
+        rootView2.clipsToBounds = false*/
+        //rootView1.translatesAutoresizingMaskIntoConstraints = false
+        //rootView2.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            guideView.leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor),
+            guideView.trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            guideView.topAnchor.constraint(equalTo: superview.topAnchor),
+            guideView.bottomAnchor.constraint(equalTo: superview.keyboardLayoutGuide.bottomAnchor)
+        ])
+        
+    }
+
+}
